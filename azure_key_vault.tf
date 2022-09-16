918f11f5b459d743b7fc284d27aa7971c5494454
John McDonough <movinalot@gmail.com>
2022-09-16 11:05:13 -0400

    update outputs

--- azure_key_vault.tf
+++ azure_key_vault.tf
@@ -3,10 +3,6 @@ data "azurerm_key_vault" "key_vault" {
   resource_group_name = "FTNT-Training-Mgmt"
 }
 
-output "vault_uri" {
-  value = data.azurerm_key_vault.key_vault.vault_uri
-}
-
 data "azurerm_key_vault_secret" "webhook_uri" {
   name         = "tf-challenge-webhook"
   key_vault_id = data.azurerm_key_vault.key_vault.id
@@ -15,9 +11,4 @@ data "azurerm_key_vault_secret" "webhook_uri" {
 data "azurerm_key_vault_secret" "fgt_password" {
   name         = "fgt-password"
   key_vault_id = data.azurerm_key_vault.key_vault.id
-}
-
-output "secret_value" {
-  value     = data.azurerm_key_vault_secret.webhook_uri.value
-  sensitive = true
 }
